680047472629578dcfbcd2fed4f5ef299f590003
eneperezv <eneperezv@gmail.com>
2024-11-07 22:47:51 -0500

    Inclusion de documentacion inicial de las clases
     Changes to be committed:
    	modified:   spendify.api/src/main/java/com/enp/spendify/api/dto/BudgetDto.java
    	modified:   spendify.api/src/main/java/com/enp/spendify/api/dto/CategoryDto.java
    	modified:   spendify.api/src/main/java/com/enp/spendify/api/dto/ExpenseDto.java

--- spendify.api/src/main/java/com/enp/spendify/api/dto/CategoryDto.java
+++ spendify.api/src/main/java/com/enp/spendify/api/dto/CategoryDto.java
@@ -1,8 +1,21 @@
 package com.enp.spendify.api.dto;
 
-import java.util.List;
+/*
+ * @(#)CategoryDto.java 1.0 07/11/2024
+ * 
+ * El código implementado en este formulario esta protegido
+ * bajo las leyes internacionales del Derecho de Autor, sin embargo
+ * se entrega bajo las condiciones de la General Public License (GNU GPLv3)
+ * descrita en https://www.gnu.org/licenses/gpl-3.0.html
+ */
 
-import com.enp.spendify.api.model.Expense;
+/**
+ * @author eliezer.navarro
+ * @version 1.0
+ * @since 1.0
+ */
+
+import java.util.List;
 
 public class CategoryDto {
 	
